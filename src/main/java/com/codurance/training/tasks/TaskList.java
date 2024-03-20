8e3db1d184b33af7ddb4c5a1d2c1066248a3ef78
Ralap88 <lydia88na@gmail.com>
2024-03-20 22:44:38 +0800

    [Refactoring] Revised repository to use repositoryPeer, not done.

--- src/main/java/com/codurance/training/tasks/TaskList.java
+++ src/main/java/com/codurance/training/tasks/TaskList.java
@@ -35,8 +35,8 @@ public final class TaskList implements Runnable {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
 
-        TaskListRepositoryPeer taskListRepositoryPeer = new InMemoryRepositoryPeer();
-        TaskListRepository repository = new InMemoryToDoListRepository(taskListRepositoryPeer);
+        TaskListRepositoryPeer taskListRepositoryPeer = new InMemoryTaskListRepositoryPeer();
+        TaskListRepository repository = new InMemoryTaskListRepository(taskListRepositoryPeer);
 
         if (repository.findById(ProjectId.of(DEFAULT_TASK_LIST_ID)).isEmpty()) {
             repository.save(new ProjectList(ProjectId.of(DEFAULT_TASK_LIST_ID)));
